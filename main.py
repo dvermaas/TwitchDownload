a7140e6cd497ae329440c1b06dbafba374f1ac82
dverm <dvermaas@live.com>
2024-01-05 20:54:48 +0100

    new commands

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ def main():
     for video in vod_dict.get('videos')[::-1]:
         print(video.get("title"), video.get("publishedAt"), video.get("id"))
 
-    # twitch-dl download 1643027074 -q source --output "{id}_{title}.{format}"
+    # twitch-dl download 2016481831 -q source --output "{date}_{id}_{title}.{format}"
+    # ffmpeg -i 2023-12-28_2016481831_future mayor of los santos yung dab.mkv -c:v libaom-av1 -strict 2023-12-28_2016481831_future mayor of los santos yung dab.av1
+    # ffmpeg -hwaccel cuda -i "2023-12-28_2016481831_future mayor of los santos yung dab.mkv" -c:v libx264 -aom-params lossless=1  "futuremayorh264.mkv"
+    # ffmpeg -i "2023-12-28_2016481831_future mayor of los santos yung dab.mkv" -c:v h264_nvenc -b:v 8000k output.mp4
 
 
 if __name__ == "__main__":
