056f992792f492fb44bafd0505bad77060cc315f
dverm <dvermaas@live.com>
2024-01-21 21:11:00 +0100

    add chat downloads

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 import json
 import subprocess
+import os
+
+working_dir = "F:\\"
+
+
+def download_vods(online_vods: set, folder="Vods"):
+    saved_vods = set([filename.split("_")[0] for filename in os.listdir(working_dir+folder)])
+    todo_vods = online_vods - saved_vods
+    if not todo_vods:
+        print("No vods found")
+        return
+    ts = " ".join(todo_vods)
+    dowload_vods_cmd = f'twitch-dl download {ts}' + ' -q source --output "{id}_{date}.{format}" --max-workers 12'
+    subprocess.call(dowload_vods_cmd, cwd=working_dir+folder)
+
+
+def download_chats(online_vods: set, folder="Chats"):
+    saved_chats = set([filename.split(".")[0] for filename in os.listdir(working_dir+folder)])
+    todo_chats = online_vods - saved_chats
+    print(len(todo_chats), "chats found")
+    for chat_id in todo_chats:
+        download_chat_cmd = f"chat_downloader https://www.twitch.tv/videos/{chat_id} --output {chat_id}.log"
+        subprocess.call(download_chat_cmd, cwd=working_dir+folder)
+
 
 def main():
-    command = 'twitch-dl videos moonmoon --json --game "Grand Theft Auto V " --all'
-    vod_dict = subprocess.check_output(command, shell=True, encoding="utf-8").splitlines()[1]
+    list_vods_cmd = 'twitch-dl videos moonmoon --json --game "Grand Theft Auto V " --all'
+    vod_dict = subprocess.check_output(list_vods_cmd, cwd=working_dir, shell=True, encoding="utf-8").splitlines()[1]
     vod_dict = dict(json.loads(vod_dict))
 
-    print(f"Got {vod_dict.get('videos')}")
-    for video in vod_dict.get('videos')[::-1]:
-        print(video.get("title"), video.get("publishedAt"), video.get("id"))
-
-    # twitch-dl download 2016481831 -q source --output "{date}_{id}_{title}.{format}"
-    # ffmpeg -i 2023-12-28_2016481831_future mayor of los santos yung dab.mkv -c:v libaom-av1 -strict 2023-12-28_2016481831_future mayor of los santos yung dab.av1
-    # ffmpeg -hwaccel cuda -i "2023-12-28_2016481831_future mayor of los santos yung dab.mkv" -c:v libx264 -aom-params lossless=1  "futuremayorh264.mkv"
-    # ffmpeg -i "2023-12-28_2016481831_future mayor of los santos yung dab.mkv" -c:v h264_nvenc -b:v 8000k output.mp4
+    online_vods = set([video.get("id") for video in vod_dict.get('videos')[::-1]])
+    download_vods(online_vods)
+    download_chats(online_vods)
 
 
 if __name__ == "__main__":
