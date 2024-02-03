6590af933f90a4c659f9c526389a95b207fa5e92
dverm <dvermaas@live.com>
2024-02-03 20:26:29 +0100

    restructure code

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
+import os
 import json
 import subprocess
-import os
 
-working_dir = "F:\\"
+working_dir = "G:\\"
 
 
 def download_vods(online_vods: set, folder="Vods"):
-    saved_vods = set([filename.split("_")[0] for filename in os.listdir(working_dir+folder)])
+    saved_vods = {filename.split("_")[0] for filename in os.listdir(working_dir+folder)}
     todo_vods = online_vods - saved_vods
-    if not todo_vods:
-        print("No vods found")
-        return
-    ts = " ".join(todo_vods)
-    dowload_vods_cmd = f'twitch-dl download {ts}' + ' -q source --output "{id}_{date}.{format}" --max-workers 12'
-    subprocess.call(dowload_vods_cmd, cwd=working_dir+folder)
+    for vod_id in todo_vods:
+        dowload_vod_cmd = f'twitch-dl download {vod_id}' + ' -q source --output "{id}_{date}.{format}" --max-workers 8'
+        subprocess.call(dowload_vod_cmd, cwd=working_dir+folder)
 
 
 def download_chats(online_vods: set, folder="Chats"):
-    saved_chats = set([filename.split(".")[0] for filename in os.listdir(working_dir+folder)])
+    saved_chats = {filename.split(".")[0] for filename in os.listdir(working_dir+folder)}
     todo_chats = online_vods - saved_chats
-    print(len(todo_chats), "chats found")
     for chat_id in todo_chats:
         download_chat_cmd = f"chat_downloader https://www.twitch.tv/videos/{chat_id} --output {chat_id}.log"
         subprocess.call(download_chat_cmd, cwd=working_dir+folder)
@@ -33,6 +29,7 @@ def main():
     online_vods = set([video.get("id") for video in vod_dict.get('videos')[::-1]])
     download_vods(online_vods)
     download_chats(online_vods)
+    print("Done")
 
 
 if __name__ == "__main__":
