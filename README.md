c3dd01baeb532ac1fe798c7eb505344bd327988b
dverm <dvermaas@live.com>
2024-04-28 21:57:42 +0200

    bump twitch-dl version, directory generation

---
+++ README.md
@@ -0,0 +1,18 @@
+Twitch Download
+=================
+
+Python script for downloading twitch vods and chatlogs
+
+Requirements
+---------
+
+- Python 3.8 or higher
+- ffmpeg installed and on the system path
+- requirements.txt packages
+
+Quick Start
+-----------
+
+- add the broadcaster to the broadcasters-list in main.py
+- change the location to write the data to
+- run script
--- main.py
+++ main.py
@@ -2,20 +2,21 @@ import os
 import json
 import subprocess
 
-broadcasters = []
+broadcasters = ["moonmoon"]
+base_path = os.path.join(os.path.expanduser("~"), "Downloads")
 
 
 def download_vods(online_vods: set, base_dir: str, folder="Vods"):
-    path = base_dir+folder
+    path = os.path.join(base_dir, folder)
     saved_vods = {filename.split("_")[0] for filename in os.listdir(path)}
     todo_vods = online_vods - saved_vods
     for vod_id in todo_vods:
-        dowload_vod_cmd = f'twitch-dl download {vod_id}' + ' -q source --output "{id}_{date}.{format}" --max-workers 16'
+        dowload_vod_cmd = f'twitch-dl download {vod_id} -q source --output "{{id}}_{{date}}.{{format}}" --max-workers {os.cpu_count()}'
         subprocess.call(dowload_vod_cmd, cwd=path)
 
 
 def download_chats(online_vods: set, base_dir: str, folder="Chats"):
-    path = base_dir + folder
+    path = os.path.join(base_dir, folder)
     saved_chats = {filename.split(".")[0] for filename in os.listdir(path)}
     todo_chats = online_vods - saved_chats
     for chat_id in todo_chats:
@@ -25,9 +26,14 @@ def download_chats(online_vods: set, base_dir: str, folder="Chats"):
 
 def main():
     for broadcaster in broadcasters:
-        working_dir = f"E:\\{broadcaster}\\"
-        list_vods_cmd = f'twitch-dl videos {broadcaster} --json --game "Grand Theft Auto V " --all'
-        vod_dict = subprocess.check_output(list_vods_cmd, cwd=working_dir, shell=True, encoding="utf-8").splitlines()[1]
+        working_dir = os.path.join(base_path, broadcaster)
+        if not os.path.exists(working_dir):
+            os.makedirs(working_dir)
+            os.makedirs(os.path.join(working_dir, "Vods"))
+            os.makedirs(os.path.join(working_dir, "Chats"))
+
+        list_vods_cmd = f'twitch-dl videos {broadcaster} --json --all'
+        vod_dict = subprocess.check_output(list_vods_cmd, cwd=working_dir, shell=True, encoding="utf-8").splitlines()[0]
         vod_dict = dict(json.loads(vod_dict))
         online_vods = set([video.get("id") for video in vod_dict.get('videos')[::-1]])
         download_vods(online_vods, working_dir)
